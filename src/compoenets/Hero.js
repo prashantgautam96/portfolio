0a2e0153405e4c12c759860c83b23ff063d94f0a
prashantgautam96 <2020ucs0114@iitjammu.ac.in>
2023-04-15 18:35:39 +0530

    hero modified

--- src/compoenets/Hero.js
+++ src/compoenets/Hero.js
@@ -5,14 +5,49 @@ import "./hero.css"
 export const Hero = () => {
   return (
     <div className='hero' > 
-        <div className='left-container'>
-        <h1>Hi, Nice to meet you!! </h1>
-        
-        </div>
-        
-        <div className='right-container'>
-        “Welcome to my portfolio! I’m a web developer who specializes in creating beautiful and functional websites.”
-        </div>
+        <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+            <div className="flexslider js-fullheight">
+                <ul className="slides">
+                <li>
+                    <div className="overlay" />
+                    <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                        <div className="slider-text-inner js-fullheight">
+                            <div className="desc">
+                            <h1>Hi! <br />I'm Prashant Gautam</h1>
+                           <p><a >Download CV</a></p>
+                            </div>
+                        </div>
+                        </div>
+                        
+                    </div>
+                    </div>
+                </li>
+                 
+                    <div className="overlay" />
+                    <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                        <div className="slider-text-inner js-fullheight">
+                            <div className="desc">
+                            <p>
+                            “I am a web developer with experience in React, Node.js, PostgreSQL, MongoDB, computer vision and natural language processing (NLP). Ans passionate about creating beautiful and functional web applications that solve real-world problems. In their free time, they enjoy hiking and reading.”
+                            </p>
+                           <p><a >Explore GitHub</a></p>
+                            </div>
+                        </div>
+                        </div>
+                        
+                    </div>
+                    </div>
+                
+  
+                </ul>
+            </div>
+        </section>
+      </div>
        
         
     </div>
